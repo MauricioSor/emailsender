cbcba4083e600cf25cebec5cda512ce6cc545834
Mauricio Soria <mauricioutn2017@gmail.com>
2024-03-15 18:39:03 -0300

    feature: email req added

--- src/controllers/email.controllers.js
+++ src/controllers/email.controllers.js
@@ -3,18 +3,17 @@ const resend = new Resend('re_XohbZVTH_BSg8bWzSDsAkQdQTCL1zrQEX');
 
 export const email = async (req, res) => {
     try {
-        const { nombre, mensaje } = req.body;
+        const { nombre, mensaje,contacto } = req.body;
 
-        if (!nombre || !mensaje) {
+        if (!nombre || !mensaje || !contacto) {
             return res.status(400).json({ error: "Nombre, correo y mensaje son campos obligatorios." });
         }
         const { data, error } = await resend.emails.send({
             from: `Maubot <onboarding@resend.dev>`,
             to: "mauricioutn2017@gmail.com",
-            subject: `Hola tienes un correo de ${nombre}!`,
+            subject: `Hola tienes un correo de ${nombre}! <br> Su contacto es ${correo}`,
             text: mensaje,
         });
-
         if (error) {
             return res.status(400).json({ error });
         }
